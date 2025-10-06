c999a3f506089037286661ac6bfba4cb23f279a3
smatval0712 <sergiopracticas5@gmail.com>
2025-10-06 10:40:35 +0200

    cambio .gitignore

---
+++ practica2/src/ejercicio1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+
+    public static void main (String[] args){
+        //Crea un array de 10 posiciones de números pedidos por teclado
+        Scanner sc = new Scanner(System.in);
+        int[] arrayInt = new int[10];
+        for (int i=0;i<10;i++){
+            sc.nextInt();
+        }
+        //Muestra por consola el indice y el valor
+        //indice -> valor
+    }
+}
