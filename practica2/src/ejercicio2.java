8f96a2220373ebb96f6a495e7772462fdd816c92
smatval0712 <sergiopracticas5@gmail.com>
2025-10-06 13:15:36 +0200

    Ejercicio 2 terminado

---
+++ practica2/src/ejercicio2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main (String[] args){
+        //Crea un array de numeros enteros
+        int[] n = {88,33,15,54,64,99,77};
+        //Muestra por consola los elementos del array separados por un espacio
+        for (int i=0; i<n.length; i++){
+            System.out.print(i+"->"+n[i]+" ");
+        }
+    }
+}
