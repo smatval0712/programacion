e11df3fa8146156a0466b2926d612c4c7f5e9f62
smatval0712 <sergiopracticas5@gmail.com>
2025-10-06 13:50:50 +0200

    Ejercicio 3 terminado

---
+++ practica2/src/ejercicio3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ejercicio3 {
+    public static void main (String[] args){
+        //Crea un array de 5 numeros enteros
+        int[] n = {12,55,32,78,5};
+        int menor = n[0];
+        //Muestra por consola el más pequeño de ellos
+        for (int i=0; i<n.length; i++){
+            if (n[i]< menor){
+                menor=n[i];
+            }
+        }
+        System.out.println("El número menor es "+menor);
+    }
+}
