2767333b2cd8aa85ebbd5371c05a2f2372149024
smatval0712 <sergiopracticas5@gmail.com>
2025-10-06 10:44:07 +0200

    Ejercicio 1 hecho a medias

--- practica2/src/ejercicio1.java
+++ practica2/src/ejercicio1.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
 public class ejercicio1 {
-
-
     public static void main (String[] args){
         //Crea un array de 10 posiciones de números pedidos por teclado
         Scanner sc = new Scanner(System.in);
         int[] arrayInt = new int[10];
         for (int i=0;i<10;i++){
-            sc.nextInt();
+            System.out.println("Introduce el siguiente valor");
+            arrayInt[i] = sc.nextInt();
         }
         //Muestra por consola el indice y el valor
         //indice -> valor
