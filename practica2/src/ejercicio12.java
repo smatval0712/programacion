61b124790df5cb292a862cef864f8279b07d77a6
smatval0712 <sergiopracticas5@gmail.com>
2025-10-21 22:46:48 +0200

    ejercicio12 terminado

---
+++ practica2/src/ejercicio12.java
@@ -0,0 +1,22 @@
+//En España cada persona está identificada con un Documento Nacional
+// de Identidad (DNI) en el que figura un número y una letra, por ejemplo 56999545W.
+// Realiza un programa donde le pidas al usuario SOLO el número del dni y el programa te
+// devuelva la letra. Para calcular la letra solo tienes que dividir el número del DNI
+// entre 23, el resto de esta división se corresponde con la posición de la letra en el
+// abecedario. Utiliza un array para guardar CADA letra del abecedario.
+
+import java.util.Scanner;
+
+public class ejercicio12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce su DNI sin letra: ");
+        //Array con cada letra del abecedario como nos dice el ejercicio
+        String[] abecedario = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        //Variables creadas dni para el dni que introducimos por teclado y n almacena la posicion del array para mostrar la letra
+        int dni= sc.nextInt();
+        int n= dni%23;
+        //Muestra por pantalla la letra
+        System.out.println("La letra de su DNI es: " +abecedario[n]);
+    }
+}
