558b64335c62cdfd8fc738821e64f0598c73b4a8
Checo <sergiopracticas5@gmail.com>
2025-10-24 21:41:36 +0200

    ejercicio 14 corregido quitando el .contains

--- practica2/src/ejercicio14.java
+++ practica2/src/ejercicio14.java
@@ -16,27 +16,30 @@ public class ejercicio14 {
         //Con un bucle while pedimos nombres por teclado y lo añadimos al array, a no ser que pongamos la palabra fin que significara el final del bucle
         while (true) {
             System.out.println("Introduce nombres, cuando quieras parar introduce fin: ");
-            String p = sc.nextLine();
-            if (p.equals("fin")) {
+            String nombre = sc.nextLine();
+            if (nombre.equals("fin")) {
                 break;
             } else {
-                lista.add(p);
+                lista.add(nombre);
             }
         }
-        //Pedimos un nombre por teclado para buscarlo en el array y lo almacenamos en la variable busqueda
-        System.out.println("Introduce un nombre a buscar: ");
-        String busqueda = sc.nextLine();
-        //Con un if con el valor array (lista).contains que es un atributo booleano que compara y devuelve verdadero falso
-        //si lo que queremos buscar está en el array
-        if(lista.contains(busqueda)){
-            //creamos una variable que se llama pos que guardara la posicion del array mediante el atributo .indexOf que devuelve
-            // la posicion en la que se encuentra o -1 si no esta en el array
-            int pos = lista.indexOf(busqueda);
-            //Imprimimos el nombre y la posicion o no esta en la lista si no se encuentra.
-            System.out.println("El nombre buscado está en la lista, en la posición "+pos);
+        System.out.println("Lista terminada");
+        System.out.println("Introduce un nombre para comprobar si está en la lista y en qué posición: ");
+        String busqueda= sc.nextLine();
+        int encontrado=0;
+        int posicion=0;
+        for (int i=0; i< lista.size();i++) {
+            if (busqueda.equals(lista.get(i))) {
+                encontrado++;
+                posicion= i;
+                break;
+            }
+        }
+        if (encontrado>0){
+            System.out.println("El nombre sí está en la lista, en la posición: " +posicion);
         }
         else {
-            System.out.println("El nombre buscado no está en la lista");
+            System.out.println("El nombre no está en la lista");
         }
     }
 }
