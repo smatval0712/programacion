362b91a2d6ef076247bc37894dd7e941ed20c367
smatval0712 <sergiopracticas5@gmail.com>
2025-11-17 13:03:42 +0100

    practica poo incompleta

--- PracticaPOO/src/Usuario.java
+++ PracticaPOO/src/Usuario.java
@@ -3,66 +3,70 @@ public class Usuario {
     private String nombre;
     private String apellidos;
     private String direccion;
-    private String codigopostal;
+    private Integer codigopostal;
     private String email;
     private String contraseña;
+
     //constructor
-    public Usuario (String nom, String ape, String dir,String codpos, String em, String pass) {
-        nombre = nom;
-        apellidos = ape;
-        direccion = dir;
-        codigopostal = codpos;
-        email = em;
-        contraseña = pass;
+    public Usuario (String nom, String ape, String dir,Integer codpos, String em, String pass) {
+        this.nombre = nom;
+        this.apellidos = ape;
+        this.direccion = dir;
+        this.codigopostal = codpos;
+        this.email = em;
+        this.contraseña = pass;
     }
     //get para todos los atributos
     public String getNombre(){
-        return nombre;
-    }
-    public String getApellidos(){
-        return apellidos;
-    }
-    public String getDireccion(){
-        return direccion;
-    }
-    public String getCodigopostal(){
-        return codigopostal;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getContraseña(){
-        return contraseña;
-    }
-
-//set para todos los atributos
-
-
-    public String setNombre(String nuevoNombre) {
-        this.nombre = nuevoNombre;
         return this.nombre;
     }
-    public String setApellidos(String nuevoApellidos) {
-        this.apellidos = nuevoApellidos;
+    public String getApellidos(){
         return this.apellidos;
     }
-    public String setDireccion(String nuevaDireccion) {
-        this.direccion = nuevaDireccion;
+    public String getDireccion(){
         return this.direccion;
     }
-    public String setEmail(String nuevoEmail) {
-        if (email.contains("@")) {
+    public Integer getCodigopostal(){
+        return this.codigopostal;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getContraseña(){
+        return this.contraseña;
+    }
+
+    //set para todos los atributos
+    public void setNombre(String nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+    public void setApellidos(String nuevoApellidos) {
+        this.apellidos = nuevoApellidos;
+    }
+    public void setDireccion(String nuevaDireccion) {
+        this.direccion = nuevaDireccion;
+    }
+    public void setEmail(String nuevoEmail) {
+        if (nuevoEmail.contains("@")) {
             this.email = nuevoEmail;
         }
         else {
             System.out.println("Error:Email inválido. No contiene @");
         }
-        return nuevoEmail;
     }
-    public String setCodigopostal(String nuevoCodigopostal) {
+    public void setCodigopostal(String nuevoCodigopostal) {
         this.codigopostal = codigopostal;
     }
     public void setContraseña(String nuevaContraseña) {
         this.contraseña = contraseña;
+    }
+
+    //Metodo de autenticación
+    public boolean checkUsuario(String email, String pass){
+        return this.email.equals(email) && this.contraseña.equals(pass);
+    }
+    //Metodo ToString
+    public String toString(){
+        return "El nombre es" +this.nombre + ", el apellido es " +this.apellidos + ", y el email es " +this.email;
     }
 }
