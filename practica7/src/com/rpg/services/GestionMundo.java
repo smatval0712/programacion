b5d1407cc8814e6ea3ef6feae08fb289b4ec17a8
Sergio <sergiopracticas5@gmail.com>
2026-03-16 10:40:57 +0100

    Practica7: defensa con rutas cambiadas a formato linux

--- practica7/src/com/rpg/services/GestionMundo.java
+++ practica7/src/com/rpg/services/GestionMundo.java
@@ -40,9 +40,9 @@ public class GestionMundo {
     public void cargarTodo() throws FormatoInvalidoException {
         this.listaCiudades = txt.leerTxt();
 
-        this.personajes = jhelper.leerJSON("practica7/ficheros/personajes.json", Personaje.class);
+        this.personajes = jhelper.leerJSON("/home/checo/IdeaProjects/programacion/practica7/ficheros/personajes.json", Personaje.class);
 
-        this.listaItem = jhelper.leerJSON("practica7/ficheros/Items.json", Item.class);
+        this.listaItem = jhelper.leerJSON("/home/checo/IdeaProjects/programacion/practica7/ficheros/Items.json", Item.class);
 
         for (Item item : listaItem) {
             this.mapItems.put(item.getId(), item);
@@ -71,7 +71,7 @@ public class GestionMundo {
 
     //Guardar cammbios llama a escribirJson
     public void guardarCambios() throws RPGDataException {
-        jhelper.escribirJson("practica7/ficheros/personajes.json", this.personajes);
+        jhelper.escribirJson("/home/checo/IdeaProjects/programacion/practica7/ficheros/personajes.json", this.personajes);
     }
 
     public void equipamientoInteligente(List<Ciudad> listaCiudades, List<Personaje> personajes, List<Item> listaItem) throws FormatoInvalidoException {
--- practica7/src/com/rpg/utils/LoggerCustom.java
+++ practica7/src/com/rpg/utils/LoggerCustom.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 public class LoggerCustom {
     //Funcion para escribir en errores.log
     public void escribirLog(String mensaje){
-        String path= "practica7/ficheros/errores.log";
+        String path= "ficheros/errores.log";
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(path,true))){
             writer.write("[" + LocalDateTime.now() + "] " + mensaje);
             writer.newLine();
